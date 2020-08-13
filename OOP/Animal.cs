e398290cfcaccdce1f8c9cd9d22f98d7f4e43ddb
PodiWan <38519328+PodiWan@users.noreply.github.com>
2020-08-13 15:37:20 +0300

    Add files via upload

---
+++ OOP/Animal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOP
+{
+    partial class Program
+    {
+        class Animal
+        {
+            //attributes
+            private int _numberOfFeet;
+            private bool _isMammal;
+            private string _name;
+
+            //setters
+            public void SetNumberOfFeet(int nr)
+            {
+                if (nr >= 0)
+                    _numberOfFeet = nr;
+                else
+                    Console.WriteLine("The number of feet has to be greater or equal to 0.");
+            }
+
+            public void SetMammalProperty(bool isMammal)
+            {
+                _isMammal = isMammal;
+            }
+
+            public void SetName(string name)
+            {
+                _name = name;
+            }
+
+            //getters
+            public int GetNumberOfFeet()
+            {
+                return _numberOfFeet;
+            }
+
+            public bool GetMammalProperty()
+            {
+                return _isMammal;
+            }
+            
+            public string GetName()
+            {
+                return _name;
+            }
+
+
+            //other methods
+            public void Name()
+            {
+                if (_isMammal == true)
+                    Console.WriteLine("{0} is a mammal", _name);
+                else
+                    Console.WriteLine("{0} is an animal", _name);
+            }
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.SetNumberOfFeet(4);
+            Console.WriteLine(animal.GetNumberOfFeet());
+            animal.SetName("Darius");
+            animal.Name();
+
+            Animal animal2 = new Animal();
+            animal2.SetMammalProperty(true);
+            animal2.SetName("David");
+            animal2.Name();
+        }
+    }
+}
